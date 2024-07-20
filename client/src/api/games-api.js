e6c1ddd4ef68d434ff356095c64907a437d372bf
lz3sv <voichev@yahoo.com>
2024-07-21 02:52:27 +0300

    list all games

--- client/src/api/games-api.js
+++ client/src/api/games-api.js
@@ -1,4 +1,9 @@
 import * as request from './requester'
 
-const BASE_URL='http://localhost:3030/jasonstore/games'
-export const getAll = () => request.get(BASE_URL)
+const BASE_URL='http://localhost:3030/jsonstore/games'
+export const getAll = async () => {
+    const response= await request.get(BASE_URL)
+    const games=Object.values(response)
+    console.log(games)
+    return games
+}
--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,22 +1,19 @@
-import { useEffect } from "react"
-import requester from "../../api/requester"
-import * as request from '../../api/requester'
+import { useEffect, useState } from "react"
+
 import * as gamesAPI from '../../api/games-api'
+import GameListItem from "./game-list-item"
 
 export default function Catalog(){
-    const [games.setGames]=useState ([])
+    const [games,setGames]=useState ([])
     
     useEffect(()=> {
 
         gamesAPI.getAll()
             .then(result=>setGames(result))
        //( async() =>{
-
              //const gamesResults = await requester('GET','http://localhost:3030/jsonstore/games')
              //const gamesResults = await request.get('http://localhost:3030/jsonstore/games')
              //console.log(gamesResult)
-
-
         //})()
     },[])
     
@@ -26,32 +23,8 @@ export default function Catalog(){
         <section id="catalog-page">
         <h1>All Games</h1>
         {/* <!-- Display div: with information about every game (if any) --> */}
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg"/>
-                <h6>Action</h6>
-                <h2>Cover Fire</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
 
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg"/>
-                <h6>Action</h6>
-                <h2>Zombie lang</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg"/>
-                <h6>Action</h6>
-                <h2>MineCraft</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-        </div>
+            {games.map(game => <GameListItem key={game._id}{...game}/>)}   
 
         {/* <!-- Display paragraph: If there is no games  --> */}
         <h3 className="no-articles">No articles yet</h3>
