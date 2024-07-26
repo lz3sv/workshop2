12de6aacb21e13a1a8f3f589fd5d997b4c77fa70
lz3sv <voichev@yahoo.com>
2024-07-26 22:29:59 +0300

    Auth global state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
-import {Routes, Route} from 'react-router-dom'
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
@@ -6,6 +8,8 @@ import Register from './components/register/Register'
 import Catalog from './components/catalog/Catalog'
 import Create from './components/create/Create'
 import Details from './components/details/Details'
+import { AuthContext } from './context/AuthContext'
+
 
 
 
@@ -13,22 +17,34 @@ import Details from './components/details/Details'
 
 
 function App() {
+  const [authState, setAuthState] = useState({})
 
+  const changeAuthState=(state)=> {
+    setAuthState(state)
+  }
 
+  const contextData={
+    email: authState.email,
+    accessToken: authState.accessToken,
+    isAuthenticated: !!authState.email,
+    changeAuthState,
+  }
   return (
-    <div id="box">
-      <Header/>
-      <main id="main-content">
-        <Routes>
-          <Route path= '/' element={<Home/>}/>
-          <Route path= '/login' element={<Login/>}/>
-          <Route path= '/register' element={<Register/>}/>
-          <Route path= '/catalog' element={<Catalog/>}/>
-          <Route path= '/catalog/:gameId/details' element={<Details/>}/>
-          <Route path= '/create' element={<Create/>}/>
-        </Routes>
-      </main>
-    </div>
+    <AuthContext.Provider value={contextData}>
+      <div id="box">
+        <Header />
+        <main id="main-content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/catalog/:gameId/details' element={<Details />} />
+            <Route path='/create' element={<Create />} />
+          </Routes>
+        </main>
+      </div>
+    </AuthContext.Provider>
   )
 }
 
--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,9 +1,14 @@
+import { useContext } from "react"
 import { login } from "../api/auth-api"
+import { AuthContext } from "../context/AuthContext"
 
 export const useLogin=()=>{
+    const {changeAuthState}= useContext(AuthContext)
+    
     const loginHandler=async (email, password)=> {
             const result= await login(email, password)
-            console.log(result)
+            changeAuthState(result)
+            //console.log(result)
     }
     return loginHandler
 }
