95f7318c55ecb3eb1b49e590272eee39bed150d0
lz3sv <voichev@yahoo.com>
2024-07-26 14:06:12 +0300

    Update Login.jsx

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,58 +2,62 @@ import { useNavigate } from "react-router-dom"
 import { useLogin } from "../../hooks/useAuth"
 import { useForm } from "../../hooks/useForm"
 
-export default function Login(){
+const initialValues = { email: '', password: '' }
 
-    const login=useLogin()
-    const navigate=useNavigate()
-    const {values, changeHandler,submitHandler}=useForm(
-        { email:'', password:'' }, 
-        async ({email, password})=> {
-            try{
-                await login(email, password)
-                navigate('/')
-            }catch(err){
-                console.log(err.message)
-            }
-        }  
-    )
-    
-    
-    
-        return (
-       
-        
-    <section id="login-page" className="auth">
-        <form id="login" onSubmit={submitHandler}>
+export default function Login() {
 
-            <div className="container">
-                <div className="brand-logo"></div>
-                <h1>Login</h1>
-                <label htmlFor="email">Email:</label>
-                <input 
-                    type="email" 
-                    id="email" 
-                    name="email" 
-                    value={values.email}
-                    onChange={changeHandler}
-                    placeholder="Sokka@gmail.com"
-                />
+    const login = useLogin()
+    const navigate = useNavigate()
+    const loginHandler = async ({ email, password }) => {
+        try {
+            await login(email, password)
+            navigate('/')
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+    const { 
+        values, 
+        changeHandler, 
+        submitHandler 
+    } = useForm(initialValues, loginHandler)
 
-                <label htmlFor="login-pass">Password:</label>
-                <input 
-                    type="password" 
-                    id="login-password" 
-                    name="password"
-                    value={values.password}
-                    onChange={changeHandler}
-                />
-                <input type="submit" className="btn submit" value="Login"/>
-                <p className="field">
-                    <span>If you dont have profile click <a href="#">here</a></span>
-                </p>
-            </div>
-        </form>
-    </section>
-    
+
+
+    return (
+
+
+        <section id="login-page" className="auth">
+            <form id="login" onSubmit={submitHandler}>
+
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={values.email}
+                        onChange={changeHandler}
+                        placeholder="Sokka@gmail.com"
+                    />
+
+                    <label htmlFor="login-pass">Password:</label>
+                    <input
+                        type="password"
+                        id="login-password"
+                        name="password"
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
+                    <input type="submit" className="btn submit" value="Login" />
+                    <p className="field">
+                        <span>If you dont have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>
+
     )
 } 
